c3f887d078bdd0104733fb1c6dd08e916cba72bd
golden-brown <yudha.personals@gmail.com>
2024-09-27 16:50:20 +0700

    new update

--- js/autoplay.js
+++ js/autoplay.js
@@ -1,13 +1,1 @@
-var video = document.querySelector('video');
-
-if (video !== undefined) {
-  video.then(_ => {
-    video.play();
-    // Autoplay started!
-  }).catch(error => {
-    // Autoplay was prevented.
-    // Show a "Play" button so that user can start playback.
-    video.muted = false;
-    video.play();
-  });
-}
+document.getElementById('video').autoplay = true;
